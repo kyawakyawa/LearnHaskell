8292167143cfa8900f5361c5530e6fddd5fc817e
kyawakyawa <kyawashell@gmail.com>
2020-09-06 12:32:58 +0900

    ch02 test.hs

---
+++ ch02/test.hs
@@ -0,0 +1,18 @@
+double x = x + x
+
+quadruple x = double (double x)
+
+a = b + c
+  where
+    b = 1
+    c = 2
+
+-- 一行で書く場合
+-- a = b + c where b = 1; c = 2
+
+d = a * 2
+
+{-
+  まとめて
+  コメント
+-}
