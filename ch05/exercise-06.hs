f98a6f19cfe5a1d0a1a9cc50c0e26cafde57e947
kyawakyawa <kyawashell@gmail.com>
2020-09-27 11:42:50 +0900

    use init

--- ch05/exercise-06.hs
+++ ch05/exercise-06.hs
@@ -2,4 +2,4 @@ factors :: Int -> [Int]
 factors n = [x | x <- [1 .. n], n `mod` x == 0]
 
 perfects :: Int -> [Int]
-perfects n = [x | x <- [1 .. n], x == sum (tail (reverse (factors x)))]
+perfects n = [x | x <- [1 .. n], x == sum (init (factors x))]
